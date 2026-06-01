a0d2dcaa73f23a9c301a215e812f39f7fbcc7d5d
MeiTozawa <tozawamei726@gmail.com>
2026-06-01 13:30:41 +0900

    feat: HealthComponent

---
+++ MagicCat/program/Charactor/Health/HealthComponent.cpp
@@ -0,0 +1,37 @@
+﻿module;
+
+module HealthComponent;
+#include <functional>
+
+class HealthComponent : public IDamageable
+{
+    int health = 100;
+    bool isDead = false;
+
+public:
+    std::vector<std::function<void()>> OnDeathEvent;
+    
+    void TakeDamage(int damage) override
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            isDead = true;
+            for (auto i : OnDeathEvent)
+            {
+                i();
+            }
+        }
+    }
+
+    void RegisterDeathCallback(const std::function<void()> callback) override
+    {
+        if (callback != nullptr)
+           OnDeathEvent.push_back(callback);
+    }
+
+    bool IsDead() override
+    {
+        return isDead;
+    }
+};
---
+++ MagicCat/program/Charactor/Health/HealthComponent.ixx
@@ -0,0 +1,18 @@
+﻿module;
+
+export module HealthComponent;
+#include <functional>
+
+export class IDamageable
+{
+public:
+    virtual ~IDamageable() const = 0;
+    
+    virtual void TakeDamage(int damage) = 0;
+
+    virtual void RegisterDeathCallback(std::function<void()> callback) = 0;
+    
+    virtual bool IsDead() = 0;
+};
+
+

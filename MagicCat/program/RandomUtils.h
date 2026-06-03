fc0628da2210b6a460938c5b316041759a38ac47
MeiTozawa <tozawamei726@gmail.com>
2026-06-03 15:11:56 +0900

    feat: RandomUtils.h

---
+++ MagicCat/program/RandomUtils.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <random>
+
+class Random {
+private:
+    static std::mt19937& GetEngine() {
+        thread_local std::random_device rd;
+        thread_local std::mt19937 gen(rd());
+        return gen;
+    }
+
+public:
+    Random() = delete;
+
+    template<typename... T>
+    static T RandomSelection(T ...weights) {
+        std::discrete_distribution dist({weights...});
+        return dist(GetEngine());
+    }
+
+};
